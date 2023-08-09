db40712fe098bdde44684c6fc031106e4fa10fb5
Hung Luu <duchung.luu@live.com>
2023-08-09 12:24:19 +0700

    Add more code for githuba ction

--- function_app.py
+++ function_app.py
@@ -1,9 +1,10 @@
+import azure.functions as func
 import logging
 
-import azure.functions as func
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="HttpTrigger")
+def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
